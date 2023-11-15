37fead9e4733f8ac8404f4d3b0fd23f07b48fafb
User <User@MSI>
2023-11-15 17:10:03 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace BookDownloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.button1.Font = new System.Drawing.Font("Cambria", 13.2F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(82, 623);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Cambria", 13.2F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(852, 623);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 47);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Cambria", 11F, System.Drawing.FontStyle.Bold);
+            this.linkLabel1.Location = new System.Drawing.Point(81, 332);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(170, 22);
+            this.linkLabel1.TabIndex = 11;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "LINCOLN LETTERS";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Cambria", 11F, System.Drawing.FontStyle.Bold);
+            this.linkLabel2.Location = new System.Drawing.Point(309, 332);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(223, 22);
+            this.linkLabel2.TabIndex = 12;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "NARRATIVE OF THE LIFE";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Cambria", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(335, 374);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(683, 227);
+            this.richTextBox1.TabIndex = 13;
+            this.richTextBox1.Text = "";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Font = new System.Drawing.Font("Cambria", 11F, System.Drawing.FontStyle.Bold);
+            this.linkLabel3.Location = new System.Drawing.Point(574, 332);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(203, 22);
+            this.linkLabel3.TabIndex = 14;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "THE SCARLET LETTER";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(82, 374);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(169, 227);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::BookDownloader.Properties.Resources.Book4;
+            this.pictureBox4.Location = new System.Drawing.Point(849, 72);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(169, 227);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::BookDownloader.Properties.Resources.Book3;
+            this.pictureBox3.Location = new System.Drawing.Point(590, 72);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(169, 227);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::BookDownloader.Properties.Resources.Book2;
+            this.pictureBox2.Location = new System.Drawing.Point(335, 72);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(169, 227);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BookDownloader.Properties.Resources.Book11;
+            this.pictureBox1.Location = new System.Drawing.Point(82, 73);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(169, 226);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.Font = new System.Drawing.Font("Cambria", 11F, System.Drawing.FontStyle.Bold);
+            this.linkLabel4.Location = new System.Drawing.Point(860, 332);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(154, 22);
+            this.linkLabel4.TabIndex = 15;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "MISS MEREDITH";
+            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(79, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(310, 36);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "BOOK DOWNLOADER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1099, 682);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel4);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookDownloader
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> bookUrlsl;
+        private bool bookSelected = false;
+        private string selectedBookTitle = ""; 
+        public Form1()
+        {
+            InitializeComponent();
+         
+
+            bookUrlsl = new Dictionary<string, string>
+            {
+                { "LINCOLN LETTERS" , "ftp://gutenberg.pglaf.org/8/1/1/8110/8110.txt"  },
+                { "NARRATIVE OF THE LIFE" , "ftp://gutenberg.pglaf.org/2/23/23-0.txt" },
+                { "THE SCARLET LETTER" , "ftp://gutenberg.pglaf.org/3/33/33-0.txt"},
+                { "MISS MEREDITH" , "ftp://gutenberg.pglaf.org/5/9/9/9/59990/59990.txt" },
+
+              
+            };
+
+            pictureBox1.Tag = "LINCOLN LETTERS";
+            pictureBox2.Tag = "NARRATIVE OF THE LIFE";
+            pictureBox3.Tag = "THE SCARLET LETTER";
+            pictureBox4.Tag = "MISS MEREDITH"; 
+        }
+
+  
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            string selectedBookTitle = pictureBox1.Tag.ToString();
+            string bookText = Get_Data(selectedBookTitle); 
+            richTextBox1.Text = bookText;
+
+            linkLabel1.Text = selectedBookTitle;
+
+            //pop up the selected book cover in picturebox6
+            DisplayBookCover(selectedBookTitle); 
+
+            bookSelected = true;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+            string selectedBookTitle = pictureBox2.Tag.ToString();
+            string bookText = Get_Data(selectedBookTitle);
+            richTextBox1.Text = bookText;
+
+            linkLabel2.Text = selectedBookTitle;
+
+          
+            DisplayBookCover(selectedBookTitle);
+            bookSelected = true;
+        }
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            string selectedBookTitle = pictureBox3.Tag.ToString();
+            string bookText = Get_Data(selectedBookTitle);
+            richTextBox1.Text = bookText;
+
+            linkLabel3.Text = selectedBookTitle;
+
+          
+            DisplayBookCover(selectedBookTitle);
+
+            bookSelected = true;
+        }
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            string selectedBookTitle = pictureBox4.Tag.ToString();
+            string bookText = Get_Data(selectedBookTitle);
+            richTextBox1.Text = bookText;
+
+            linkLabel4.Text = selectedBookTitle;
+
+          
+            DisplayBookCover(selectedBookTitle);
+
+            bookSelected = true;
+        }
+
+
+        private string Get_Data(string bookTitle)
+        {
+            string result = string.Empty;
+           
+
+            if (bookUrlsl.ContainsKey(bookTitle))
+            {
+                string bookUrl = bookUrlsl[bookTitle];
+
+                try
+                {
+                    FtpWebRequest request = (FtpWebRequest)WebRequest.Create(bookUrl);
+                    request.Method = WebRequestMethods.Ftp.DownloadFile;
+
+                    using (FtpWebResponse response = (FtpWebResponse)request.GetResponse())
+                    using (Stream responseStream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(responseStream))
+                    {
+                        result = reader.ReadToEnd();
+                    }
+
+
+                    // Save file locally (optional)
+                    using (StreamWriter file = File.CreateText($"{bookTitle}.txt"))
+                    {
+                        file.WriteLine(result);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error downloading book: " + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Book URL not found.");
+            }
+
+            return result;
+        }
+
+        private void DisplayBookCover(string bookTitle)
+        {
+            switch (bookTitle)
+            {
+                case "LINCOLN LETTERS":
+                    pictureBox6.Image = Properties.Resources.Book1; 
+                    break;
+                case "NARRATIVE OF THE LIFE":
+                    pictureBox6.Image = Properties.Resources.Book2; 
+                    break;
+                case "THE SCARLET LETTER": 
+                    pictureBox6.Image= Properties.Resources.Book3;
+                    break;
+                case "MISS MEREDITH":
+                    pictureBox6.Image = Properties.Resources.Book4; 
+                    break;
+                default:
+                    pictureBox6.Image = null;
+                    break;
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string selectedBookTitle = linkLabel1.Text;
+
+            string bookText = Get_Data(selectedBookTitle); 
+            richTextBox1.Text = bookText;
+
+            linkLabel1.Text = selectedBookTitle;
+
+            DisplayBookCover (selectedBookTitle);
+
+            bookSelected = true; 
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string selectedBookTitle = linkLabel2.Text; 
+
+            string bookText = Get_Data(selectedBookTitle);
+
+            richTextBox1.Text = bookText;
+
+            linkLabel2.Text = selectedBookTitle;
+
+            DisplayBookCover(selectedBookTitle);
+
+            bookSelected = true;
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string selectedBookTitle = linkLabel3.Text;
+
+            string bookText = Get_Data(selectedBookTitle);
+
+            richTextBox1.Text = bookText;
+
+            linkLabel3.Text = selectedBookTitle;
+
+            DisplayBookCover(selectedBookTitle);
+
+            bookSelected = true;
+        }
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string selectedBookTitle = linkLabel4.Text;
+
+            string bookText = Get_Data(selectedBookTitle);
+
+            richTextBox1.Text = bookText;
+
+            linkLabel4.Text = selectedBookTitle;
+
+            DisplayBookCover(selectedBookTitle);
+
+            bookSelected = true;
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!bookSelected)
+            {
+               
+                MessageBox.Show("Please select a book before downloading.");
+                return;
+            }
+
+            foreach (string bookTitle in bookUrlsl.Keys)
+            {
+                string bookText = Get_Data(bookTitle);
+               // richTextBox1.Text = bookText;
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "Text Files (*.txt | *.txt | PDF File (*.pdf) | *.pdf",
+                    Title = "Save Book Text", 
+                    FileName = $"{bookTitle}.txt"     
+                };
+
+                DialogResult dialogResult = saveFileDialog.ShowDialog();
+
+                // If the user cancels, exit the loop and return from the button1_Click event
+                if (dialogResult != DialogResult.OK)
+                {
+
+                   return; 
+                   
+                }
+
+                string fileName = saveFileDialog.FileName;
+                if (saveFileDialog.FilterIndex == 1)
+                {
+                    SaveTextFile(fileName, bookText);
+                }
+                else if (saveFileDialog.FilterIndex == 2)
+                {
+                    SavePdfFile(fileName, bookText);
+                }
+
+                richTextBox1.Text = bookText;
+            }
+        }
+
+        private void SaveTextFile(string fileName , string bookText)
+        {
+            try
+            {
+                using (StreamWriter file = File.CreateText(fileName))
+                {
+                    file.Write(bookText);
+                }
+                MessageBox.Show("Book text saved as a TXT file successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving book text as a TXT file: " + ex.Message);
+            }
+        }
+
+        private void SavePdfFile(string fileName, string bookText)
+        {
+            //I am using iTextSharp library (to do this i installed iTextSharp in NuGet package)
+            try
+            {
+                iTextSharp.text.Document pdfDoc = new iTextSharp.text.Document();
+                using (FileStream fs = new FileStream(fileName, FileMode.Create))
+                {
+                    iTextSharp.text.pdf.PdfWriter.GetInstance(pdfDoc, fs);
+                    pdfDoc.Open();
+                    pdfDoc.Add(new iTextSharp.text.Paragraph(bookText));
+                    pdfDoc.Close();
+                }
+                MessageBox.Show("Book text saved as a PDF file successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving book text as a PDF file: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
